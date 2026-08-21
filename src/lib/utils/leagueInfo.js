d6420e111a503f50966a882bd00e17bd4d6e2b7c
sickened36 <121896441+sickened36@users.noreply.github.com>
2026-08-20 21:25:10 -0500

    Update leagueInfo.js

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -7,8 +7,7 @@ export const enableBlog = false; // requires VITE_CONTENTFUL_ACCESS_TOKEN and VI
 
 /*   STEP 2   */
 export const homepageText = 
-  <p>This... is... FANTASY FOOOSBALLLLL</p>
-';
+  <p>This... is... FANTASY FOOOSBALLLLL</p>;
 
 /*   STEP 3   */
 /*
